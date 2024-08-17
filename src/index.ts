ceb3b46b13ced89c2d46a23d81249a6f95187b48
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-08-17 19:00:24 +0300

    chore: Add Express server with health check route

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import healthCheckRouter from "./routes/health-check.routes";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+// enable cors
+app.use(cors());
+// parse json request body
+app.use(express.json());
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api", healthCheckRouter);
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at ${PORT}`);
+});
