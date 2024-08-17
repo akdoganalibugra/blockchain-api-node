65cd6a2e8e79eb11a0c44b86b7dfc9b97556f658
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-08-17 03:05:14 +0300

    chore: Update TestToken contract to include mint function

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("TestToken", "TTK") {
+    constructor(uint256 initialSupply) ERC20("TestToken", "TT") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 }
