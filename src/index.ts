353112941200cb12e9e4a1c44d85e0c5e5cf7657
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-08-18 03:03:31 +0300

    feat: Add reward routes for managing rewards and winners

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import healthCheckRouter from "./routes/health-check.routes";
+import rewardRouter from "./routes/reward.routes";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api", healthCheckRouter);
+app.use("/api/rewards", rewardRouter);
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at ${PORT}`);
